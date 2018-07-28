169a4cb5c792af1401a41df76ded5d68605d3e9e
shimamiki <tamifull@jcom.zaq.ne.jp>
2018-07-28 20:26:20 +0900

    StepAsideUnityChanはじめてのコミット
    画面外に出たアイテムを消せないため、みてもらうためコミットした

---
+++ Assets/BackWallController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackWallController : MonoBehaviour {
+
+	//Unityちゃんのオブジェクト
+	private GameObject unitychan;
+	//UnityちゃんとBackWallの距離
+	private float difference;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//Unityちゃんのオブジェクトを取得
+		this.unitychan = GameObject.Find("unitychan");
+		//unityちゃんとBackWallのzの差
+		this.difference = unitychan.transform.position.z - this.transform.position.z;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//unityチャンの位置に合わせてBackWallの位置を移動する
+		this.transform.position = new Vector3(0,this.transform.position.y, this.unitychan.transform.position.z - difference);
+		
+	}
+
+
+	//トリガーモードでほかのオブジェクトと接触した場合
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag" ||  other.gameObject.tag == "CoinTag") {
+			Debug.Log ("あああああ");
+			Destroy (other.gameObject);
+		}
+
+	}
+}
+	
---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour {
+
+
+	public GameObject[] coins;
+	public GameObject[] cars;
+	public GameObject[] cones;
+
+	public GameObject UnityChan;
+
+	// Use this for initialization
+	void Start () {
+
+		coins = GameObject.FindGameObjectsWithTag("CoinTag");
+		for (int i = 0; i < coins.Length; i++) {
+			Debug.Log (coins[i].name );
+		}
+		cars = GameObject.FindGameObjectsWithTag("CarTag");
+		cones = GameObject.FindGameObjectsWithTag("TrafficConeTag");
+
+		UnityChan = GameObject.Find ("unitychan");
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		for (int i = 0; i < coins.Length; i++) {
+			if (UnityChan.transform.position.z > coins [i].transform.position.z) {
+				Destroy (coins[i].gameObject);
+			}
+		}
+
+		for (int i = 0; i < cars.Length; i++) {
+			if (UnityChan.transform.position.z > cars [i].transform.position.z) {
+				Destroy(cars[i].gameObject);
+			}
+		}
+
+		for (int i = 0; i < cones.Length; i++) {
+			if (UnityChan.transform.position.z > cones [i].transform.position.z) {
+				Destroy (cones[i].gameObject);
+			}
+		}
+
+
+
+		
+	}
+}
